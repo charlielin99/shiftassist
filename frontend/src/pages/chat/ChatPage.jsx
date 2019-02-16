8875e24f184270f078ae51e07a931ba2a521205a
Alex Kreidler <alexkreidler2020@gmail.com>
2019-02-16 17:01:38 -0500

    add chat page

---
+++ frontend/src/pages/chat/ChatPage.jsx
@@ -0,0 +1,12 @@
+import PageNavWrapper from "../PageNavWrapper/PageNavWrapper";
+import React from "react";
+
+export default function Patient(props) {
+  return (
+    <PageNavWrapper>
+      <div>
+        <h1>Chat Page</h1>
+      </div>
+    </PageNavWrapper>
+  );
+}
