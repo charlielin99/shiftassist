1e4bfe768a3f097be53a8bb07bc0da249bc62a2a
Alex Kreidler <alexkreidler2020@gmail.com>
2019-02-15 22:36:38 -0500

    add splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { Component } from "react";
+
+// import logo from "./logo.svg";
+import Particles from "react-particles-js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="splash container">
+        <Particles className="particles">
+          <h1>Hi</h1>
+        </Particles>
+        <div className="centered">
+          <h1>ShiftChange</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            A mobile, machine learning based solution to one of the largest
+            healthcare industry issues.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <a href="#">Background</a>
+        </div>
       </div>
     );
   }
