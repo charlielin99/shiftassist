9a06b7b49bad892b998feecc28ff6e6c038fcb44
Timothy Higinbottom <timhigins@gmail.com>
2019-02-17 11:15:01 -0500

    Auto stash before merge of "master" and "origin/master"

--- frontend/src/areas/patient/PatientContainer.jsx
+++ frontend/src/areas/patient/PatientContainer.jsx
@@ -2,32 +2,71 @@ import React, { Component } from "react";
 import { getChangeReports, getPatients } from "../../services/DataService";
 
 import Patient from "./Patient";
+import { Query } from "react-apollo";
+import { Spin } from "antd";
+import gql from "graphql-tag";
 
 export default class PatientContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { user: {}, loading: true };
     if (props.match) {
-      getPatients(props.match.params.patientId).then(data => {
-        this.setState({ user: data, loading: false });
+      const pID = props.match.params.patientId;
+      //   getPatients().then(data => {
+      //     this.setState({ user: data, loading: false });
 
-        let p = [];
-        for (const event of data.history) {
-          p.push(getChangeReports(event));
-        }
-        Promise.all(p).then(data => {
-          this.setState({ shiftChanges: data });
-        });
-      });
+      //     let p = [];
+      //     for (const event of data.history) {
+      //       p.push(getChangeReports(event));
+      //     }
+      //     Promise.all(p).then(data => {
+      //       this.setState({ shiftChanges: data });
+      //     });
+      //   });
     }
   }
   render() {
     return (
-      <Patient
-        user={this.state.user}
-        shiftChanges={this.state.shiftChanges}
-        {...this.props}
-      />
+      <Query
+        variables={{ id: this.props.match.params.patientId }}
+        query={gql`
+          query GetPatient($id: String!) {
+            patient(id: $id) {
+              firstName
+              lastName
+              email
+              phone
+              address
+              gender
+              history {
+                _id
+                outgoingNurse {
+                  lastName
+                }
+                incomingNurse {
+                  lastName
+                }
+                dateTime {
+                  t
+                }
+              }
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          //   if (loading) return <Spin></Spin>;
+          //   if (error) return <p>Error :(</p>;
+          console.log(data);
+          return (
+            <Patient
+              user={Object.keys(data).length != 0 && data.patient}
+              shiftChanges={this.state.shiftChanges}
+              {...this.props}
+            />
+          );
+        }}
+      </Query>
     );
   }
 }
--- frontend/src/areas/patients/PatientsContainer.jsx
+++ frontend/src/areas/patients/PatientsContainer.jsx
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
 
 import Patients from "./Patients";
-import { getPatients } from "../../services/DataService";
+import { Query } from "react-apollo";
+import { Spin } from "antd";
+import gql from "graphql-tag";
 
-export default class PatientsContainer extends Component {
+// import { getPatients } from "../../services/DataService";
+
+// export default class PatientsContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { users: [], loading: true };
+//     getPatients().then(data => {
+//       this.setState({ users: data, loading: false });
+//     });
+//   }
+//   render() {
+//     return <Patients users={this.state.users} loading={this.state.loading} />;
+//   }
+// }
+
+export default class PatientsContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { users: [], loading: true };
-    getPatients().then(data => {
-      this.setState({ users: data, loading: false });
-    });
   }
+
   render() {
-    return <Patients users={this.state.users} loading={this.state.loading} />;
+    return (
+      <Query
+        query={gql`
+          query patients {
+            patients(limit: 100) {
+              _id
+              firstName
+              lastName
+              gender
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          //   if (loading) return <Spin></Spin>;
+          //   if (error) return <p>Error :(</p>;
+          console.log(data);
+          return <Patients patients={data.patients} loading={loading} />;
+        }}
+      </Query>
+    );
   }
 }
